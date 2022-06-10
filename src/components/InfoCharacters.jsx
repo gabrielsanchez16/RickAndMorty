6be2876d08b7d5e231db0eb393a2a8f6fff0aa26
gabriel sanchez <gabrielpelota.8@gmail.com>
2022-06-10 14:20:01 -0500

    changestyle

--- src/components/InfoCharacters.jsx
+++ src/components/InfoCharacters.jsx
@@ -1,39 +1,55 @@
-import axios from 'axios'
-import React from 'react'
-import { useState, useEffect } from 'react'
+import axios from "axios";
+import React from "react";
+import { useState, useEffect } from "react";
 
+const InfoCharacters = ({ resident }) => {
+  const [character, setCharacter] = useState();
 
-const InfoCharacters = ({resident}) => {
+  useEffect(() => {
+    axios
+      .get(resident)
+      .then((res) => setCharacter(res.data))
+      .catch((error) => console.log(error));
+  }, []);
 
-    const [character, setCharacter] = useState()
-
-    useEffect(() => {
-     axios.get(resident)
-     .then(res => setCharacter(res.data))
-     .catch(error => console.log(error))
-    }, [])
-    
-    console.log(character)
+  const circlechange = {
+    Alive: "green",
+    Dead: "red",
+    unknown: "purple",
+  };
+  console.log(character);
 
   return (
-    <article className='characters'>
-      <div className='container-img'>
+    <article className="characters">
+      <div className="container-img">
         <img src={character?.image} alt="" />
         <div>
-          <span className='status'><b>Status:</b> {character?.status}</span>
+          <span className="status">
+            {character?.status}{" "}
+            <div
+              className="Circle"
+              style={{ background: circlechange[character?.status] }}
+            ></div>
+          </span>
         </div>
       </div>
-        <h3>{character?.name}</h3>
-        <div className='description'>
-            <ul>
-                <li><b>Species:</b> {character?.species}</li>
-                
-                <li><b>Origin:</b> {character?.origin.name}</li>
-                <li><b>Appearances:</b> {character?.episode?.length}</li>
-            </ul>
-        </div>
-    </article>
-  )
-}
+      <h3>{character?.name}</h3>
+      <div className="description">
+        <ul>
+          <li>
+            <b>Species:</b> {character?.species}
+          </li>
 
-export default InfoCharacters
+          <li>
+            <b>Origin:</b> {character?.origin.name}
+          </li>
+          <li>
+            <b>Appearances:</b> {character?.episode?.length}
+          </li>
+        </ul>
+      </div>
+    </article>
+  );
+};
+
+export default InfoCharacters;
